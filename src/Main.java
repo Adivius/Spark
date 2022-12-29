265490deda1d233d06636041b3141c22654b984a
Adivius <adivius@chaos-siegen.de>
2022-12-29 23:20:24 +0100

    Fixed not showing cause when connection not possible

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         UI.init();
         try {
-
             String ip = args[0];
             int port = Integer.parseInt(args[1]);
             String name =args[2];
@@ -14,8 +15,15 @@ public class Main {
             UI.sparkClient = client;
 
         } catch (Exception e) {
-            System.out.println("Error starting client: " + e.getMessage());
+            UI.print("Error starting client: " + Arrays.toString(e.getStackTrace()));
+            System.out.println("Error starting client: " + Arrays.toString(e.getStackTrace()));
             e.printStackTrace();
+            try{
+                Thread.sleep(5000);
+            }catch (Exception ex){
+                System.exit(0);
+            }
+            System.exit(0);
         }
     }
 }
